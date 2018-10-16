2f75debcf1a29242c92657ac478e8fcf7aef2467
hiscaler <hiscaler@gmail.com>
2018-10-16 16:14:22 +0800

    添加域名验证

---
+++ src/yadjet/validators/DomainValidator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace yadjet\validators;
+
+use yii\validators\Validator;
+
+/**
+ * 判断是否为有效的域名
+ *
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class DomainValidator extends Validator
+{
+
+    public $allowEmpty = true;
+
+    private function isValidDomain($value)
+    {
+        return preg_match("/[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?/", $value) ? true : false;
+    }
+
+    public function validateAttribute($object, $attribute)
+    {
+        $value = $object->$attribute;
+        if ($this->allowEmpty && $this->isEmpty($value)) {
+            return;
+        }
+
+        if (!$this->isValidDomain($value)) {
+            $message = $this->message !== null ? $this->message : "{$value} 不是一个有效的域名。";
+            $this->addError($object, $attribute, $message);
+        }
+    }
+
+    public function validateValue($value)
+    {
+        if (!$this->isValidDomain($value)) {
+            return [$this->message, []];
+        }
+
+        return null;
+    }
+
+}
---
+++ tests/DomainValidatorTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace yadjet\helpers;
+
+require __DIR__ . '/../vendor/autoload.php';
+require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+
+use PHPUnit\Framework\TestCase;
+use yadjet\validators\DomainValidator;
+
+/**
+ * Class ArrayHelperTest
+ *
+ * @package yadjet\helpers
+ * @author hiscaler <hiscaler@gmail.com>
+ */
+class DomainValidatorTest extends TestCase
+{
+
+    public function testValid()
+    {
+        $validator = new DomainValidator();
+        $this->assertEquals($validator->validate('www.example.com'), true);
+        $this->assertEquals($validator->validate('example.com'), true);
+        $this->assertEquals($validator->validate('www.abc.example.com'), true);
+        $this->assertEquals($validator->validate('a@com'), false);
+        $this->assertEquals($validator->validate('.com'), false);
+        $this->assertEquals($validator->validate('abc'), false);
+        $this->assertEquals($validator->validate('11.22.33'), true);
+    }
+
+}
